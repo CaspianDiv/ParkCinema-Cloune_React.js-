5167c7ae2fa1e02949fc04632931f0c04177ffd1
CaspianDiv <sahinjafarov996@gmail.com>
2026-06-11 21:26:04 +0400

    fixed import Authcontext

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import DataContext from './context/DataContext.jsx';
-import AuthContext from './context/authContext.jsx';
+import AuthContext from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
